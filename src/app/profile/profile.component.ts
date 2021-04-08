2e29b40dbecfb84ac774d32ed6a16e6474edf265
GoobIsGabe <gabe.harp1996@gmail.com>
2021-04-08 16:21:53 -0400

    starting out again

---
+++ src/app/profile/profile.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
+export class ProfileComponent implements OnInit {
+  public show:boolean = false;
+  public buttonName:any = 'Show';
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  toggle() {
+    this.show = !this.show;
+
+    // CHANGE THE NAME OF THE BUTTON.
+    if(this.show)  
+      this.buttonName = "Hide";
+    else
+      this.buttonName = "Show";
+  }
+}
